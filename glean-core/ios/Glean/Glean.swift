8f670ce52b14977b225732ab01ee633aab923a3c
Jan-Erik Rediger <jrediger@mozilla.com>
2020-07-21 15:36:11 +0200

    Suppress warning by ignoring return value

--- glean-core/ios/Glean/Glean.swift
+++ glean-core/ios/Glean/Glean.swift
@@ -118,7 +118,7 @@ public class Glean {
             }
 
             if let debugViewTag = self.debugViewTag {
-                self.setDebugViewTag(debugViewTag)
+                _ = self.setDebugViewTag(debugViewTag)
             }
 
             if self.logPings {
